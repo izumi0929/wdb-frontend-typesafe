e2c7a27712b0cf6c6ec3f9e6bf1a6b8d125f0ed2
izumeeeel <izumi.watahiki@gmail.com>
2022-09-06 08:38:36 +0900

    add withRef button

--- src/ComponentPropsWithoutRef.tsx
+++ src/ComponentPropsWithoutRef.tsx
@@ -1,25 +1,38 @@
-import React from "react";
-import styles from "./styles.module.css";
+import React, { MutableRefObject, RefObject, useRef } from 'react'
+import styles from './styles.module.css'
 
 type Props = {
-  theme: "dark" | "light";
-} & React.ComponentPropsWithoutRef<"button">;
+  theme: 'dark' | 'light'
+} & React.ComponentPropsWithoutRef<'button'>
 
 export const Button = (props: Props) => {
-  return <button {...props} className={styles[props.theme]} />;
-};
+  return <button {...props} className={styles[props.theme]} />
+}
 
-export const ButtonContainer = () => (
-  <>
-    <Button
-      theme="dark"
-      onClick={() => {
-        // onClick イベントハンドラー型定義は
-        // React.ComponentPropsWithoutRef<"button"> に含まれる
-      }}
-    >
-      +1
-    </Button>
-    ```
-  </>
-);
+type PropsWithRef = {
+  theme: 'dark' | 'light'
+} & React.ComponentPropsWithRef<'button'>
+
+export const ButtonWithRef = (props: PropsWithRef) => {
+  return <button {...props} className={styles[props.theme]} />
+}
+
+export const ButtonContainer = () => {
+  const ref = useRef() as MutableRefObject<HTMLButtonElement>
+  return (
+    <>
+      <Button
+        theme='dark'
+        onClick={() => {
+          // onClick イベントハンドラー型定義は
+          // React.ComponentPropsWithoutRef<"button"> に含まれる
+        }}
+      >
+        +1
+      </Button>
+      <ButtonWithRef theme='light' ref={ref}>
+        ref?
+      </ButtonWithRef>
+    </>
+  )
+}
